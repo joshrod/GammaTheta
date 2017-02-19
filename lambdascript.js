95e1d25bb19cbd7af8c3530e346eed7a1f9d12a8
Joshua Rodriguez <joshrodriguez17@gmail.com>
2017-02-19 12:47:40 -0600

    Completed the layout of index, along with its CSS and JS. Need to add media queries.

---
+++ lambdascript.js
@@ -0,0 +1,42 @@
+
+    var slideNumber = 1;
+
+    showSlides(slideNumber);
+
+    setInterval(automatic, 10000);
+
+    function automatic() {
+      slideNumber++;
+      showSlides(slideNumber);
+    }
+
+    function plusSlides(n) {
+      showSlides(slideNumber += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideNumber = n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("slides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideNumber = 1} 
+      if (n < 1) {slideNumber = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideNumber-1].style.display = "block"; 
+      dots[slideNumber-1].className += " active";
+    }
+
+
+$(document).ready(function() {
+
+  $('.slides a').smoothScroll();
+
+});
