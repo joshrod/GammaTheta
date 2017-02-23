023a6f78d12e0c3cabeea7d56f7812c189d6455e
Joshua Rodriguez <joshrodriguez17@gmail.com>
2017-02-22 18:27:09 -0600

    Added Interested and Lineage Pages, completed their responsiveness and JS.

--- lambdascript.js
+++ lambdascript.js
@@ -1,3 +1,44 @@
+$(function() {
+
+  var form = $('#contactform');
+
+  var formMessages = $('#formmessages');
+
+  $(form).submit(function(e) {
+    e.preventDefault();
+
+    var formData = $(form).serialize();
+
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    })
+    .done(function(response) {
+      $(formMessages).removeClass('error');
+      $(formMessages).addClass('success');
+
+      $(formMessages).text(response);
+
+      $('#name').val('');
+      $('#email').val('');
+      $('#message').val('');
+    })
+    .fail(function(data) {
+      $(formMessages).removeClass('success');
+      $(formMessages).addClass('error');
+
+      if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+      } else {
+        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+      }
+    });
+
+  });
+
+});
+
 $(document).ready(function() {
 
   $('.slides a').smoothScroll();
@@ -62,5 +103,7 @@ $(document).ready(function() {
   });
 
   $('.fancybox').fancybox();
+
+  $('.linelink').fancybox();
 
 });
